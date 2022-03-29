1ae8b12090da9decb525096dbe55d7b82840a887
Philipp Glira <philipp.glira@ait.ac.at>
2022-03-29 09:50:33 +0200

    Add gemfile and update serve script

--- serve_pglira.sh
+++ serve_pglira.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# Currently (2022-03-29) this isn't working because the Gemfile is missing:
+# bundle install -> Could not locate Gemfile
 
 set -eu pipefail
 
 # Install jekyll first: https://jekyllrb.com/docs/installation/ubuntu/
 
 cd /data/repos/pglira.github.io
+bundle install
 bundle exec jekyll serve --trace
