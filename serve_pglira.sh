697a444544070dc464d0fa1d5d71eb9ff9da5f5f
Philipp Glira <philipp.glira@ait.ac.at>
2022-06-09 07:33:17 +0200

    Add install of jekyll and vscode task

--- serve_pglira.sh
+++ serve_pglira.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-# Currently (2022-03-29) this isn't working because the Gemfile is missing:
-# bundle install -> Could not locate Gemfile
 
 set -eu pipefail
 
-# Install jekyll first: https://jekyllrb.com/docs/installation/ubuntu/
+# Install jekyll prerequisites first with:
+# sudo apt-get install ruby-full build-essential zlib1g-dev
+
+export GEM_HOME="$HOME/gems"
+export PATH="$HOME/gems/bin:$PATH"
+gem install jekyll bundler
 
 cd /data/repos/pglira.github.io
 bundle install
