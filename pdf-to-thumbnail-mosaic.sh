eb4bf9f17f2c2b264579ed8ef0f7fdfc2a138acf
Philipp Glira <philipp.glira@ait.ac.at>
2022-06-15 16:36:15 +0200

    Add script to generate mosaic of pdf pages

---
+++ pdf-to-thumbnail-mosaic.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
+
+pdf_filepath=$1
+mosaic_filepath=$2
+
+pdf_filename_without_dir=$(basename -- "${pdf_filepath}")
+pdf_filename_without_extension="${pdf_filename_without_dir%.*}"
+
+thumbnails_tmp_dirpath=/tmp/pdf-to-thumbnail-mosaic
+thumbnails_filepath=${thumbnails_tmp_dirpath}/${pdf_filename_without_extension}.png
+
+if [ -d "${thumbnails_tmp_dirpath}" ]; then
+  rm -rf "${thumbnails_tmp_dirpath}"
+fi
+mkdir -p "${thumbnails_tmp_dirpath}"
+
+pdftoppm -png "${pdf_filepath}" "${thumbnails_filepath}"
+
+montage -geometry "20%x20%+10+10>" "${thumbnails_tmp_dirpath}/*.png" -frame 4 -tile 4x -background none -shadow "${mosaic_filepath}"
+convert -trim +repage "${mosaic_filepath}" "${mosaic_filepath}"
